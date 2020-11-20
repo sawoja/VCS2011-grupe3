cc1037a33c843e33facbea4bed6e581b9456f333
sawoja <sawoja@gmail.com>
2020-11-20 18:51:39 +0200

    Juste

--- index.php
+++ index.php
@@ -2,7 +2,41 @@
 <!-- Juste -->
 
 <!-- Čia pagrindinio puslapio vidinė dalis -->
-<h1>Cia yra pagrindinis puslapis</h1>
+<div class="main-page-photo">
+<h1>VCS restaurant</h1>
+</div>
+
+<div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php include "footer.php" ?>
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 // Rasyti komentarus, kur prasideda ir kur baigiasi kieno kodas
+
+//Justes kodas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Justes kodas end
