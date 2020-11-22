15e123697142a84b34908309f290f5b590c0869a
Edvinas3 <edvinas.milius@gmail.com>
2020-11-22 22:14:50 +0200

    updated gallery

--- gallery.php
+++ gallery.php
@@ -4,8 +4,35 @@
 
 
 <!-- Čia galerijos puslapio vidinė dalis -->
-<img src="images\Meniu(Gallery)\starter 1.jpg">
-<img src="images\Meniu(Gallery)\starter 2.jpg">
-<img src="images\Meniu(Gallery)\starter 3.jpg">
+
+<div class="container">
+
+<div class="main-page-photo">
+        <h1>Menu</h1>
+    </div>
+
+<div class="gallery">
+    <img src="https://picsum.photos/200" alt="1" width="200" height="200">
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+    <img src="https://picsum.photos/200" alt="2" width="200" height="200">
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+    <img src="https://picsum.photos/200" alt="3" width="200" height="200">
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+<img src="https://picsum.photos/200" alt="4" width="200" height="200">
+<div class="desc">Add a description of the image here</div>
+
+
+</div>
+
+</div>
 
 <?php include "footer.php" ?>
