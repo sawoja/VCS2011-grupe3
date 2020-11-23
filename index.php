317447343b41bb4454d67e246dc308cd4dddc21e
sawoja <sawoja@gmail.com>
2020-11-23 20:31:03 +0200

    Juste (header, footer, contacts)

--- index.php
+++ index.php
@@ -21,26 +21,26 @@
     </div>
 
     <div class="carousel">
+        <a class="carousel-item" href="gallery.php"><img src="https://picsum.photos/id/292/1000/800" data-toggle="tooltipped" data-position="bottom" data-tooltip="Gallery">Menu >>></a>
         <a class="carousel-item" href="contacts.php"><img src="https://picsum.photos/id/2/1000/800" data-toggle="tooltipped" data-position="bottom" data-tooltip="Contacts">Contacts >>></a>
-
         <a class="carousel-item" href="about.php"><img src="https://picsum.photos/id/225/1000/800/" data-toggle="tooltipped" data-position="bottom" data-tooltip="About">About >>></a>
 
-        <a class="carousel-item" href="gallery.php"><img src="https://picsum.photos/id/292/1000/800" data-toggle="tooltipped" data-position="bottom" data-tooltip="Gallery">Gallery >>></a>
+
 
 
 
     </div>
-        <div class="pug-picture">
+    <div class="pug-picture">
         <p>Check this out! Our chefs at work! Video down here.
         </p>
-        </div>
+    </div>
     <div class="main-page-media center-align">
 
         <a data-fancybox="gallery" href="https://www.youtube.com/watch?v=aa6GcVY7Ijg"><img src="images/play.png"></a>
 
     </div>
 
-    
+
 
 
 
