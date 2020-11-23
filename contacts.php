aa524be38f5a8de5805b756af4b186ab61b83830
sawoja <sawoja@gmail.com>
2020-11-23 21:12:49 +0200

    Juste (map)

--- contacts.php
+++ contacts.php
@@ -15,29 +15,40 @@
     </div>
 </nav>
 <div class="container">
+    <div class="row">
+        <div class="col s12 m4 l2">
+            <p></p>
+        </div>
+    </div>
     <div class="meet-team">
         <h3>Very Fancy Dining was done by our spectacular team.</h3>
     </div>
-    <div class="team-flex">
-        <div class="team">
+    <div class="team-flex row">
+        <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Daina</h4>
             <h6>Vilnius Coding School student.</h6>
         </div>
-        <div class="team">
+        <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Edvinas</h4>
             <h6>Vilnius Coding School student.</h6>
         </div>
-        <div class="team">
+        <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Justė</h4>
             <h6>Vilnius Coding School student.</h6>
         </div>
     </div>
-<div>
-    
-</div>
+
+
+    <div class="kontaktai">
+        <p>
+            Waiting You for a nice meal. Our address - Ulonų street, Vilnius. 
+        </p>
+
+        <iframe class="mapsas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1152.4951632445166!2d25.291665224989046!3d54.70979333753772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae109703e7%3A0x6f3be29852b0a444!2sUlon%C5%B3%20g.%2C%20Vilnius!5e0!3m2!1slt!2slt!4v1606157741433!5m2!1slt!2slt" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
 
 
 
