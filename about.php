85264e2100344becb8268a7d681e2d17f14216a8
Dainavai <dainavai@gmail.com>
2020-11-23 19:29:04 +0200

    About and pictures

--- about.php
+++ about.php
@@ -1,11 +1,36 @@
 
 <?php include "header.php" ?>
 
-<!-- Daina -->
+
 
 <!-- Čia apie puslapio vidinė dalis -->
-<h1>Cia yra apie mus puslapis</h1>
+<h1>We Are Gourmet Selection No 1</h1>
+<body>
+<div class="container">
+    <div class="row">
+    
+        <img class="responsive-img/100/500" src="images\Meniu(Gallery)\pasta.jpg">
 
+    </div>
+
+
+<div class="my gallery">
+    <div class="col s4 center-align">
+
+    </div>
+    <img src="images\Meniu(Gallery)\waffle.jpg">
+    <img src="images\Meniu(Gallery)\pancake.jpg">
+    <img src="images\Meniu(Gallery)\spices.jpg">
+    <img src="images\Meniu(Gallery)\fish.jpg">
+    <br>
+</div>
+
+
+</div>
+
+
+
+</body>
 
 
 <?php include "footer.php" ?>
