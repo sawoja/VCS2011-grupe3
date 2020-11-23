2cff21662dd4b2ccc229359168995f80abbda8b1
sawoja <sawoja@gmail.com>
2020-11-23 19:13:43 +0200

    Juste

--- index.php
+++ index.php
@@ -9,36 +9,38 @@
     </div>
 
     <div class="row">
-    <div class="col s12 m4 l2"><p></p></div>
-     </div>
+        <div class="col s12 m4 l2">
+            <p></p>
+        </div>
+    </div>
 
-     <div class="some-main-text">
+    <div class="some-main-text">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium saepe a voluptatem, eum aspernatur officiis ratione dolor, laboriosam vitae minus minima dicta quam porro iusto dignissimos ex error voluptatum unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium saepe a voluptatem, eum aspernatur officiis ratione dolor, laboriosam vitae minus minima dicta quam porro iusto dignissimos ex error voluptatum unde.</p>
 
 
     </div>
 
     <div class="carousel">
-        <a class="carousel-item" href="contacts.php"><img src="https://picsum.photos/id/2/1000/800" 
-        data-toggle="tooltipped" data-position="bottom" data-tooltip="Contacts">Contacts >>></a>
+        <a class="carousel-item" href="contacts.php"><img src="https://picsum.photos/id/2/1000/800" data-toggle="tooltipped" data-position="bottom" data-tooltip="Contacts">Contacts >>></a>
 
-        <a class="carousel-item" href="about.php"><img src="https://picsum.photos/id/225/1000/800/" 
-        data-toggle="tooltipped" data-position="bottom" data-tooltip="About">About >>></a>
+        <a class="carousel-item" href="about.php"><img src="https://picsum.photos/id/225/1000/800/" data-toggle="tooltipped" data-position="bottom" data-tooltip="About">About >>></a>
 
-        <a class="carousel-item" href="gallery.php"><img src="https://picsum.photos/id/292/1000/800"  
-        data-toggle="tooltipped" data-position="bottom" data-tooltip="Gallery">Gallery >>></a>
-        
+        <a class="carousel-item" href="gallery.php"><img src="https://picsum.photos/id/292/1000/800" data-toggle="tooltipped" data-position="bottom" data-tooltip="Gallery">Gallery >>></a>
 
-    
+
+
     </div>
-
+        <div class="pug-picture">
+        <p>Check this out! Our chefs at work! Video down here.
+        </p>
+        </div>
     <div class="main-page-media center-align">
 
-    <a data-fancybox="gallery" href="https://www.youtube.com/watch?v=jTx-uBdJ4GM"><img src="https://picsum.photos/id/225/1000/800"></a>
+        <a data-fancybox="gallery" href="https://www.youtube.com/watch?v=aa6GcVY7Ijg"><img src="images/play.png"></a>
 
     </div>
 
-    
+
 
 
 
