96e33067bb9eef65483e2d28e9f12c8d7144401c
Edvinas3 <edvinas.milius@gmail.com>
2020-11-23 20:58:32 +0200

    updated menu

--- gallery.php
+++ gallery.php
@@ -9,93 +9,118 @@
     <h4>Menu</h4>
 </div>
 
+<body>
 <div class="container">
 
 
-    <div class="flex">
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-             <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-    </div>
-
-    <div class="flex">
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-    </div>
-
-    <div class="flex">
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-        <div class="box">
-            <img src="images\Meniu(Gallery)\starter 1.jpg" alt="">
-            <div class="middle">
-                <div class="box-text">Meal</div>
-            </div>
-        </div>
-    </div>
-
-    <h3 class="title">Text fadeIn bottom</h3>
-  <div class="content">
-      <div class="content-overlay"></div>
-      <img class="content-image" src="images\Meniu(Gallery)\starter 1.jpg">
-      <div class="content-details fadeIn-bottom">
-        <h3 class="content-title">This is a title</h3>
-        <p class="content-text">This is a short description</p>
+    <div class="gallery">
+      <div class="content">
+          <div class="content-overlay"></div>
+          <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+          <div class="content-details fadeIn-bottom">
+            <h3 class="content-title">Food</h3>
+            <p class="content-text">Meal Description</p>
+          </div>
+        </a>
       </div>
-    </a>
-  </div>
-      <h3 class="title">Text fadeIn bottom</h3>
-  <div class="content">
-      <div class="content-overlay"></div>
-      <img class="content-image" src="images\Meniu(Gallery)\starter 1.jpg">
-      <div class="content-details fadeIn-bottom">
-        <h3 class="content-title">This is a title</h3>
-        <p class="content-text">This is a short description</p>
-      </div>
-    </a>
-  </div>
+    </div>
+    
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+    
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+    
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+    
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+    
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
+
+    <div class="gallery">
+        <div class="content">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images\Meniu(Gallery)\platter.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">Food</h3>
+              <p class="content-text">Meal Description</p>
+            </div>
+          </a>
+        </div>
+    </div>
 
 </div>
-
+</body>
 <?php include "footer.php" ?>
