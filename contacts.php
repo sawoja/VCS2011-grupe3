406856852e91131cd362321a7d5d98a4f651bf16
sawoja <sawoja@gmail.com>
2020-11-24 19:37:21 +0200

    Juste (buttons)

--- contacts.php
+++ contacts.php
@@ -26,24 +26,24 @@
         <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Daina</h4>
-            <h6>Vilnius Coding School student.</h6>
+            <a class="waves-effect waves-light btn  red lighten-3" href="about.php"><i class="material-icons right">local_florist</i>Worked on</a>
         </div>
         <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Edvinas</h4>
-            <h6>Vilnius Coding School student.</h6>
+            <a class="waves-effect waves-light btn  red lighten-3" href="gallery.php"><i class="material-icons right">collections</i>Worked on</a>
         </div>
         <div class="team col s12 m6 l4">
             <img src="http://picsum.photos/id/63/100" alt="">
             <h4>Justė</h4>
-            <h6>Vilnius Coding School student.</h6>
+            <a class="waves-effect waves-light btn  red lighten-3" href="index.php"><i class="material-icons right">visibility</i>Worked on</a>
         </div>
     </div>
 
 
     <div class="kontaktai">
         <p>
-            Visit us and have an incredible meal. Our address - Vilnius Coding School, Ulonų street, Vilnius. 
+            Visit us and have an incredible meal. Our address - Vilnius Coding School, Ulonų street 5, Vilnius. 
         </p>
 
         <iframe class="mapsas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1152.4951632445166!2d25.291665224989046!3d54.70979333753772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae109703e7%3A0x6f3be29852b0a444!2sUlon%C5%B3%20g.%2C%20Vilnius!5e0!3m2!1slt!2slt!4v1606157741433!5m2!1slt!2slt" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
